aa525e1f84e41dae0eb2d3532a7653a8fde76cd8
Akaidkhan <akaidkhan@gmail.com>
2021-05-01 17:31:40 +0600

    form submiddion

--- portfulio/home/forms.py
+++ portfulio/home/forms.py
@@ -5,8 +5,11 @@ class user_form(forms.Form):
     # <input type="text" name="user_name" value="" required>
     user_name1 = forms.CharField(required = False, label ='First name')
 
-    user_name2 = forms.CharField(required = False, label ='Last name')
+    user_name2 = forms.CharField(required = False, label ='Last name',
+    widget = forms.TextInput( attrs= {'placeholder':'Enter your full name','style':'width:300px'} ) )
 
     #<label for="user_email">User Email</label>
     #<input type="email" name="user_email" value="" required>
     user_email = forms.EmailField()
+
+    user_dob = forms.DateField(label="Date of Birth", widget = forms.TextInput( attrs={'type':'date'}))
--- portfulio/home/views.py
+++ portfulio/home/views.py
@@ -10,4 +10,23 @@ from home import forms
 def form(request):
     new_form = forms.user_form()
     diction = {'test_form': new_form, 'heading_1': "This form is created using django library"}
+
+    if request.method == 'POST':
+        new_form = forms.user_form(request.POST)
+
+        if new_form.is_valid():
+            user_name1 = new_form.cleaned_data['user_name1']
+            user_name2 = new_form.cleaned_data['user_name2']
+            user_dob = new_form.cleaned_data['user_dob']
+            user_email = new_form.cleaned_data['user_email']
+
+            diction.update({'user_name1':user_name1})
+            diction.update({'user_name2':user_name2})
+            diction.update({'user_dob':user_dob})
+            diction.update({'user_email':user_email})
+            diction.update({'form_submited':"Yes"})
+
+
+
+
     return render(request, 'home/form.html', context=diction)
