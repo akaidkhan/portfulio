cbc46ea0af8f2ff3c58d0cd6e747aac69b321102
Akaidkhan <akaidkhan@gmail.com>
2021-05-01 22:27:11 +0600

    Form Validation

--- portfulio/home/forms.py
+++ portfulio/home/forms.py
@@ -1,15 +1,45 @@
 from django import forms
+from django.core import validators
+
+# class user_form(forms.Form):
+#     # <label for="user_name">Full Name</label>
+#     # <input type="text" name="user_name" value="" required>
+#     user_name1 = forms.CharField(required = False, label ='First name')
+#
+#     user_name2 = forms.CharField(required = False, label ='Last name',
+#     widget = forms.TextInput( attrs= {'placeholder':'Enter your full name','style':'width:300px'} ) )
+#
+#     #<label for="user_email">User Email</label>
+#     #<input type="email" name="user_email" value="" required>
+#     user_email = forms.EmailField()
+#
+#
+#     user_dob = forms.DateField(label="Date of Birth", widget = forms.TextInput( attrs={'type':'date'}))
+
+
+
+#class user_form(forms.Form):
+    #boolean_field = forms.BooleanField(required=False)
+    #field = forms.CharField(max_length=15,min_length=5)
+    #choices = (('','--SELECT OPTION--'),('1','First'),('2','Second'),('3','Third'))
+    #field = forms.ChoiceField(choices=choices, required=False)
+    #choices = (('A','A'),('B','B'),('C','C'))
+    #field = forms.ChoiceField(choices=choices, widget=forms.RadioSelect)
+    #choices = (('','--SELECT OPTION--'),('1','First'),('2','Second'),('3','Third'))
+    #field = forms.MultipleChoiceField(choices=choices, required=False)
+     #choices = (('A','A'),('B','B'),('C','C'))
+     #field = forms.MultipleChoiceField(choices=choices, widget=forms.CheckboxSelectMultiple)
+
 
 class user_form(forms.Form):
-    # <label for="user_name">Full Name</label>
-    # <input type="text" name="user_name" value="" required>
-    user_name1 = forms.CharField(required = False, label ='First name')
-
-    user_name2 = forms.CharField(required = False, label ='Last name',
-    widget = forms.TextInput( attrs= {'placeholder':'Enter your full name','style':'width:300px'} ) )
-
-    #<label for="user_email">User Email</label>
-    #<input type="email" name="user_email" value="" required>
     user_email = forms.EmailField()
+    user_vmail = forms.EmailField()
+    name = forms.CharField(validators = [validators.MaxLengthValidator(10)])
 
-    user_dob = forms.DateField(label="Date of Birth", widget = forms.TextInput( attrs={'type':'date'}))
+    def clean(self):
+        all_cleaned_data = super().clean()
+        user_email = all_cleaned_data['user_email']
+        user_vmail = all_cleaned_data['user_vmail']
+
+        if user_email != user_vmail:
+            raise forms.ValidationError("Fields Don't Match!!!!")
--- portfulio/home/views.py
+++ portfulio/home/views.py
@@ -7,26 +7,58 @@ from home import forms
 # def form(request):
 #     diction = {}
 #     return render(request,'home/form.html', context= diction)
+# def form(request):
+#     new_form = forms.user_form()
+#     diction = {'test_form': new_form, 'heading_1': "This form is created using django library"}
+#
+#     if request.method == 'POST':
+#         new_form = forms.user_form(request.POST)
+#
+#         if new_form.is_valid():
+#             user_name1 = new_form.cleaned_data['user_name1']
+#             user_name2 = new_form.cleaned_data['user_name2']
+#             user_dob = new_form.cleaned_data['user_dob']
+#             user_email = new_form.cleaned_data['user_email']
+#
+#             diction.update({'user_name1':user_name1})
+#             diction.update({'user_name2':user_name2})
+#             diction.update({'user_dob':user_dob})
+#             diction.update({'user_email':user_email})
+#             diction.update({'form_submited':"Yes"})
+#
+#
+#
+#
+#     return render(request, 'home/form.html', context=diction)
+
+#
+# def form(request):
+#     new_form = forms.user_form()
+#     diction = {'test_form': new_form, 'heading_1': "This form is created using django library"}
+#
+#     if request.method == 'POST':
+#         new_form = forms.user_form(request.POST)
+#
+#         if new_form.is_valid():
+#             diction.update({'field': new_form.cleaned_data['field']})
+#             #diction.update({'boolean_field': new_form.cleaned_data['boolean_field']})
+#             diction.update({'form_submited':"Yes"})
+#
+#
+#
+#     return render(request, 'home/form.html', context=diction)
+#
+
+
 def form(request):
     new_form = forms.user_form()
     diction = {'test_form': new_form, 'heading_1': "This form is created using django library"}
 
     if request.method == 'POST':
         new_form = forms.user_form(request.POST)
-
+        diction.update({'test_form':new_form})
         if new_form.is_valid():
-            user_name1 = new_form.cleaned_data['user_name1']
-            user_name2 = new_form.cleaned_data['user_name2']
-            user_dob = new_form.cleaned_data['user_dob']
-            user_email = new_form.cleaned_data['user_email']
-
-            diction.update({'user_name1':user_name1})
-            diction.update({'user_name2':user_name2})
-            diction.update({'user_dob':user_dob})
-            diction.update({'user_email':user_email})
+            diction.update({'field': 'Fields Match!!'})
             diction.update({'form_submited':"Yes"})
-
-
-
 
     return render(request, 'home/form.html', context=diction)
